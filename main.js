35145b5b38a4d03d34216afd05a000979d9678ef
Flynn <wphestiraid@gmail.com>
2017-10-24 17:58:24 +0900

    이벤트 핸들링 예제 테스트

--- main.js
+++ main.js
@@ -13,3 +13,32 @@ http.createServer(function(request, response) {
 }).listen(8000);
 
 console.log("Server running at http://127.0.0.1:8000");
+
+// events 모듈 사용
+var events = require('events');
+
+// EventEmitter 객체 생성
+var eventEmitter = new events.EventEmitter();
+
+// event와 EventHandler를 연동(bind)
+// eventName은 임의로 설정 가능
+var connectHandler = function connected() {
+    console.log("Connection Successful");
+
+    // data_received 이벤트 발생시키기
+    eventEmitter.emit('data_received');
+}
+
+//connection 이벤트와 connectHandler 이벤트 핸들러를 연동
+eventEmitter.on('connection', connectHandler);
+
+// data_received 이벤트와 익명 함수 연동
+// 함수를 변수 안에 담는 대신에, .on() 메소드의 인자로 직접 함수를 전달
+eventEmitter.on('data_received', function() {
+    console.log("Data Received");
+});
+
+// connection 이벤트 발생시키기
+eventEmitter.emit('connection');
+
+console.log("Program has ended");
